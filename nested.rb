b56e5a19583185fbfea535b889627ae7019af4d0
Pedro Huertas <pmh242@gmail.com>
2019-10-28 04:23:50 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -84,7 +84,11 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto][:known_for][:languages]
+    programmer_hash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    }
+    programmer_hash
     
 end
 
@@ -107,7 +111,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
+    programmer_hash
 end
 
 def adding_to_dennis
@@ -126,5 +131,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages] << 'Assembly'
+  programmer_hash
 end
