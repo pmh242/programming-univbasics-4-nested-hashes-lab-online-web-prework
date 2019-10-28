bd5beac2305c6ace5a1bdd0126edaf773d01dd9d
Pedro Huertas <pmh242@gmail.com>
2019-10-28 04:19:53 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -80,6 +84,7 @@ def adding_matz
         }
      }
 
+programmer_hash[:yukihiro_matsumoto][:known_for][:languages]
     
 end
 
